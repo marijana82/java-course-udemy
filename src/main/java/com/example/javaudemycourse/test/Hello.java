7b25719b28fa1ec4e32780030ae5364caee46d2a
marijana82 <marijana.mikolcic@gmail.com>
2024-02-15 18:01:42 +0100

    testing conditionals and logical AND operator

---
+++ src/main/java/com/example/javaudemycourse/test/Hello.java
@@ -0,0 +1,19 @@
+package com.example.javaudemycourse.test;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Marijana.");
+
+        boolean isAlien = true;
+
+        if(!isAlien) {
+            System.out.println("Hello Marijana.");
+        } else {
+            System.out.println("Hello Alien");
+        }
+
+
+
+    }
+}
---
+++ src/main/java/com/example/javaudemycourse/test/Test.java
@@ -0,0 +1,6 @@
+package com.example.javaudemycourse.test;
+
+public class Test {
+
+
+}
